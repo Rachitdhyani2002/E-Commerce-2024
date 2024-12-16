f069f61861839ab4a72061dcad5ab52f3541543d
Rachit Dhyani <rachitdhyani2002@gmail.com>
2024-12-16 11:01:54 +0530

    Update Admin.js

--- client/src/pages/admin/Admin.js
+++ client/src/pages/admin/Admin.js
@@ -3,8 +3,7 @@ import Layout from '../../components/Layout'
 import { Box } from '@mui/material';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import { Link } from 'react-router-dom';
-import Zoom from 'react-reveal/Zoom'
-import Fade from 'react-reveal/Fade'
+
 
 const Admin = () => {
     const user = JSON.parse(localStorage.getItem('user'));
@@ -14,19 +13,19 @@ const Admin = () => {
         <h1 style={{fontWeight:"100"}}>ADMIN PANEL</h1>
        <Box sx={{display:'flex',flexDirection:'row',width:"70%",alignItems:"flex-start",margin:"50px",background:"#f9f9f9",borderRadius:'20px',height:"60%",padding:'30px',border: '1px solid #ddd'}}>
           <Box sx={{display:'flex',flexDirection:'column',width:"70%",alignItems:"flex-start",margin:"40px",letterSpacing:'2px'}}>
-           <Fade left><h5 style={{color:'black',fontWeight:'100'}}>Id : {user._id}</h5></Fade>
-           <Fade right><h5 style={{color:'black',fontWeight:'100'}}>Name : {user.name}</h5></Fade>
-           <Fade left><h5 style={{color:'black',fontWeight:'100'}}>Email : {user.email}</h5></Fade>
-           <Fade right><h5 style={{color:'black',fontWeight:'100'}}>Contact : {user.contact}</h5></Fade>
-           <Fade left> <h5 style={{color:'black',fontWeight:'100'}}>Role : Admin</h5></Fade>        
+           <h5 style={{color:'black',fontWeight:'100'}}>Id : {user._id}</h5>
+           <h5 style={{color:'black',fontWeight:'100'}}>Name : {user.name}</h5>
+           <h5 style={{color:'black',fontWeight:'100'}}>Email : {user.email}</h5>
+           <h5 style={{color:'black',fontWeight:'100'}}>Contact : {user.contact}</h5>
+           <h5 style={{color:'black',fontWeight:'100'}}>Role : Admin</h5>      
           <Link to='/add-product' style={{textDecoration:'none',background:"#c30c2c",color:'white',padding:"10px",width:'300px',marginTop:"20px",borderRadius:"20px"}}>ADD PRODUCTS</Link>
         
 
           </Box>
           <Box>
-            <Zoom>
+            
             <AdminPanelSettingsIcon sx={{fontSize:"320px",color:"#c30c2c"}}/>
-            </Zoom>
+        
           </Box>
         </Box>
         </Box>
