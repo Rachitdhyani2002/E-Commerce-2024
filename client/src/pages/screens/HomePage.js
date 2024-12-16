68feb51290e5aa588d1fcfadbe5acf5cb993895e
Rachit Dhyani <rachitdhyani2002@gmail.com>
2024-12-16 10:58:29 +0530

    Update HomePage.js

--- client/src/pages/screens/HomePage.js
+++ client/src/pages/screens/HomePage.js
@@ -10,8 +10,7 @@ import image3 from '../../utils/images/smiling-african-american-man-in-cap-and-s
 import image4 from '../../utils/images/olympic-sports-digital-art-6hte0setf648eqy1.jpg'
 import image5 from '../../utils/images/Neeraj-Chopra-Diamnd-League-Trophy.jpg'
 import Cookies from 'js-cookie'
-import Fade from 'react-reveal/Fade'
-import Zoom from 'react-reveal/Zoom'
+
 
 
 
@@ -38,38 +37,38 @@ const Homepage = () => {
     <Layout>
       <Box sx={{ display: "flex", flexDirection: "column", padding: "40px", margin: "40px" }}>
         <Box sx={{ padding: "40px", margin: "20px" }}>
-          <Fade left><h3 style={{ color: "#2E2E2E", fontWeight: "100", marginBottom: "30px", fontSize: "20px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: "5px" }}>DIVER 300</h3></Fade>
-          <Fade right><h1 style={{ color: '#c30c2c', fontSize: "40px", fontWeight: "400", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: '8px' }}>PRECISION POWER POISE</h1></Fade>
-          <Fade left><h4 style={{ fontWeight: "300", fontSize: "20px", marginTop: "40px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", color: "#2E2E2E", letterSpacing: '1px' }}>The Seamaster Diver 300M continues its legacy with models made for the adventures in you. Dive deeper go further</h4></Fade>
-          <Fade right><Button onClick={handleNavigation} sx={{ border: "1px solid grey", outline: "none", padding: "10px", background: "white", color: "black", margin: "20px", width: "300px", height: "60px", '&:hover': { backgroundColor: '#c30c2c', color: "white", boxShadow: 'none', border: "none", transition: "background-color 0.8s ease-in" } }}>DISCOVER THE COLLECTION</Button></Fade>
+          <h3 style={{ color: "#2E2E2E", fontWeight: "100", marginBottom: "30px", fontSize: "20px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: "5px" }}>DIVER 300</h3>
+          <h1 style={{ color: '#c30c2c', fontSize: "40px", fontWeight: "400", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: '8px' }}>PRECISION POWER POISE</h1>
+          <h4 style={{ fontWeight: "300", fontSize: "20px", marginTop: "40px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", color: "#2E2E2E", letterSpacing: '1px' }}>The Seamaster Diver 300M continues its legacy with models made for the adventures in you. Dive deeper go further</h4>
+          <Button onClick={handleNavigation} sx={{ border: "1px solid grey", outline: "none", padding: "10px", background: "white", color: "black", margin: "20px", width: "300px", height: "60px", '&:hover': { backgroundColor: '#c30c2c', color: "white", boxShadow: 'none', border: "none", transition: "background-color 0.8s ease-in" } }}>DISCOVER THE COLLECTION</Button>
         </Box>
 
         <Box sx={{ display: "flex", flexDirection: "row", gap: "50px", marginTop: "50px", justifyContent: "center" }}>
-          <Zoom><img src={image1} width={500} height={400} style={{ marginTop: '200px', borderRadius: "10px" }} /></Zoom>
-          <Zoom><img src={image2} width={500} height={400} style={{ borderRadius: "10px" }} /></Zoom>
+          <img src={image1} width={500} height={400} style={{ marginTop: '200px', borderRadius: "10px" }} />
+          <img src={image2} width={500} height={400} style={{ borderRadius: "10px" }} />
         </Box>
       </Box>
 
       <Box sx={{ display: "flex", flexDirection: "row", padding: "30px", margin: "40px", justifyContent: "center", gap: "40px" }}>
         <Box sx={{ display: "flex", justifyContent: 'center', flexDirection: "column", width: "35%" }}>
-          <Fade left><h1 style={{ color: '#c30c2c', fontSize: "45px", fontWeight: "400", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: '8px', textAlign: "left" }}>FIND THE PERFECT GIFT</h1></Fade>
-          <Fade right><h4 style={{ fontWeight: "300", fontSize: "20px", marginTop: "40px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", color: "#2E2E2E", letterSpacing: '1px', textAlign: "left" }}>If you know who you're buying for and why, our Gift Finder will help you find the perfect present in just a few clicks.</h4></Fade>
+          <h1 style={{ color: '#c30c2c', fontSize: "45px", fontWeight: "400", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: '8px', textAlign: "left" }}>FIND THE PERFECT GIFT</h1>
+          <h4 style={{ fontWeight: "300", fontSize: "20px", marginTop: "40px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", color: "#2E2E2E", letterSpacing: '1px', textAlign: "left" }}>If you know who you're buying for and why, our Gift Finder will help you find the perfect present in just a few clicks.</h4>
           <Button onClick={handleNavigation} sx={{ border: "1px solid grey", outline: "none", padding: "10px", background: "white", color: "black", marginTop: "20px", width: "320px", height: "60px", '&:hover': { backgroundColor: '#c30c2c', color: "white", boxShadow: 'none', border: "none", transition: "background-color 0.8s ease-in" } }}>FIND YOUR GIFT</Button>
         </Box>
-        <Zoom><img src={gif} width={400} height={400} /></Zoom>
+        <img src={gif} width={400} height={400} />
       </Box>
 
       <Box sx={{ display: 'flex', flexDirection: 'row', padding: "50px", margin: "60px", height: '600px', alignItems: 'center', justifyContent: 'center' }}>
 
         <Box sx={{ p: 10, display: "flex", justifyContent: "center", alignItems: "center", flexDirection: 'column', textAlign: "left", background: 'white' }}>
-          <Zoom>
+        
             <span style={{ fontWeight: "100" }}><span style={{ color: "#c30c2c", fontSize: "80px" }}>Odysseus</span></span>
             <p style={{ marginTop: "1px", fontSize: '12px', letterSpacing: '2px' }}>India's No.1 Watches Brand </p>
-          </Zoom>
+          
         </Box>
 
         <Box sx={{ background: 'white', width: '200', margin: "20px", padding: "50px" }}>
-          <Fade right>
+          
             <h3 style={{ fontWeight: "100" }}>ABOUT US</h3>
             <p style={{ textAlign: 'justify' }}>
               At <span><span style={{ color: "#c30c2c", fontSize: "30px" }}>Odysseus</span></span> we believe a watch is more than just a timekeeper—it’s a statement of style, precision, and personality. Founded with a vision to create world-class timepieces, Titan has redefined craftsmanship and innovation since our inception. With decades of experience, we have emerged as a leading name in the global watch industry, known for our impeccable designs and unwavering commitment to quality.
@@ -80,7 +79,7 @@ const Homepage = () => {
 
               Explore the world of Titan, where time meets timelessness.
             </p>
-          </Fade>
+          
 
         </Box>
 
@@ -88,44 +87,44 @@ const Homepage = () => {
       </Box>
       <Box sx={{ display: "flex", flexDirection: "row", padding: "30px", marginBottom: "80px", justifyContent: "center", gap: "40px" }}>
         <Box sx={{ display: "flex", justifyContent: 'center', flexDirection: "column", width: "35%" }}>
-          <Fade left><h1 style={{ color: '#c30c2c', fontSize: "45px", fontWeight: "400", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: '8px', textAlign: "left" }}>NEW LOOK FOR YOUR WATCH</h1></Fade>
-          <Fade right><h4 style={{ fontWeight: "300", fontSize: "20px", marginTop: "40px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", color: "#2E2E2E", letterSpacing: '1px', textAlign: "left" }}>Your watch and strap should go together perfectly, but that doesn't mean you can't experiment with different combinations. Why not mix things up and see what happens? You may discover a look that's as individual as you are.</h4></Fade>
+          <h1 style={{ color: '#c30c2c', fontSize: "45px", fontWeight: "400", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: '8px', textAlign: "left" }}>NEW LOOK FOR YOUR WATCH</h1>
+          <h4 style={{ fontWeight: "300", fontSize: "20px", marginTop: "40px", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", color: "#2E2E2E", letterSpacing: '1px', textAlign: "left" }}>Your watch and strap should go together perfectly, but that doesn't mean you can't experiment with different combinations. Why not mix things up and see what happens? You may discover a look that's as individual as you are.</h4>
           <Button onClick={handleNavigation} sx={{ border: "1px solid grey", outline: "none", padding: "10px", background: "white", color: "black", marginTop: "20px", width: "320px", height: "60px", '&:hover': { backgroundColor: '#c30c2c', color: "white", boxShadow: 'none', border: "none", transition: "background-color 0.8s ease-in" } }}>SHOP THE COLLECTION</Button>
         </Box>
-        <Zoom><img src={gif1} width={400} height={400} /></Zoom>
+        <img src={gif1} width={400} height={400} />
       </Box>
 
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: "150px", marginBottom: "100px" }}>
-        <Fade left>
+        
           <h1 style={{ color: '#c30c2c', fontSize: "45px", fontWeight: "400", fontFamily: "font-family: 'Futura', 'Avenir', sans-serif", letterSpacing: '5px', marginBottom: '50px' }}>NEWS & STORIES</h1>
 
-        </Fade>
+        
         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: "20px" }}>
           <Box sx={{ padding: "10px", width: '30%' }}>
-            <Fade top>
+            
               <img src={image3} width={400} height={300} style={{ borderRadius: '10px' }} />
               <p style={{ color: "#c30c2c", textAlign: "left", fontSize: '12px', marginTop: "15px" }}>SPORTS</p>
               <h4 style={{ color: "#c30c2c", textAlign: "left", fontWeight: "100", marginTop: '30px' }}>WILL CHARLES WINS IN LONDON</h4>
               <p style={{ textAlign: "left", color: "#2E2E2E", marginTop: "20px" }}>Odysseus CEO WILL CHARLES has finished the golf season as the race to London Champion</p>
-            </Fade>
+            
 
           </Box>
           <Box sx={{ padding: "10px", width: '30%' }}>
-            <Fade bottom>
+            
               <img src={image4} width={400} height={300} style={{ borderRadius: '10px' }} />
               <p style={{ color: "#c30c2c", textAlign: "left", fontSize: '12px', marginTop: "15px" }}>SPORTS</p>
               <h4 style={{ color: "#c30c2c", textAlign: "left", fontWeight: "100", marginTop: '30px' }}>ODYSSEUS IS HOSTING SPORT CUP </h4>
               <p style={{ textAlign: "left", color: "#2E2E2E", marginTop: "20px" }}>After the successful host of golfing events now we are hosting charitable sports events</p>
-            </Fade>
+            
 
           </Box>
           <Box sx={{ padding: "10px", width: '30%' }}>
-            <Fade top>
+            
               <img src={image5} width={400} height={300} style={{ borderRadius: '10px' }} />
               <p style={{ color: "#c30c2c", textAlign: "left", fontSize: '12px', marginTop: "15px" }}>SPORTS</p>
               <h4 style={{ color: "#c30c2c", textAlign: "left", fontWeight: "100", marginTop: '30px' }}>NEERAJ JOINS ODYSSEUS WATCHES</h4>
               <p style={{ textAlign: "left", color: "#2E2E2E", marginTop: "20px" }}>Neeraj Chopra is going to be the new face of our ODYSSEUS watches</p>
-            </Fade>
+            
 
           </Box>
         </Box>
