e4878bb54b4a5942204a9e281b0dbc202180b73a
Rachit Dhyani <rachitdhyani2002@gmail.com>
2024-12-18 11:23:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import LogInForm from './pages/auth/LogInForm';
 import RegisterForm from './pages/auth/RegisterForm';
-import HomePage from './pages/screens/HomePage';
 import Admin from './pages/admin/Admin';
 import AddProduct from './pages/admin/AddProduct';
 import Products from './pages/screens/Products';
@@ -13,26 +12,34 @@ import Cancel from './pages/screens/Cancel';
 import Success from './pages/screens/Success';
 import About from './pages/screens/About';
 
+const HomePage = React.lazy(() => import('./pages/screens/HomePage'));
+
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<LogInForm/>}/>
-            <Route path='/register' element={<RegisterForm/>}/>
-            <Route path='/products' element={<Products/>}/>
-            <Route path='/home' element={<HomePage/>}/>
-            <Route path='/admin' element={<Admin/>}/>
-            <Route path='/cart' element={<Cart/>}/>
-            <Route path='/forget-password' element={<ForgetPasswordForm/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/add-product' element={<AddProduct/>}/>
-            <Route path='/checkout' element={<CheckOut/>}/>
-            <Route path='/cancel' element={<Cancel/>}/>
-            <Route path='/success' element={<Success/>}/>
-          </Routes>
-        </BrowserRouter>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LogInForm />} />
+          <Route path='/register' element={<RegisterForm />} />
+          <Route path='/products' element={<Products />} />
+          <Route
+            path='/home'
+            element={
+              <Suspense fallback={<p>Loading Data...</p>}>
+                <HomePage />
+              </Suspense>
+            }
+          />
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/forget-password' element={<ForgetPasswordForm />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/add-product' element={<AddProduct />} />
+          <Route path='/checkout' element={<CheckOut />} />
+          <Route path='/cancel' element={<Cancel />} />
+          <Route path='/success' element={<Success />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
