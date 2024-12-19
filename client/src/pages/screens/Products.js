818e52672f88d8f54971103fcd6f0361fa8721b0
Rachit Dhyani <rachitdhyani2002@gmail.com>
2024-12-19 11:09:04 +0530

    Updated Products.js

--- client/src/pages/screens/Products.js
+++ client/src/pages/screens/Products.js
@@ -6,7 +6,6 @@ import { Checkbox, FormControlLabel, Paper } from '@mui/material';
 import { addToCart } from '../../redux/cartSlice';
 
 
-
 const Products = () => {
   const { products, loading, error } = useSelector((state) => state.product);
   const dispatch = useDispatch();
@@ -18,13 +17,13 @@ const Products = () => {
 
   if (loading) {
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',flexDirection:"column",gap:"10px"}}>
-        <img src='https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700'/>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: "column", gap: "10px" }}>
+        <img src='https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700' width={200} height={200} />
       </div>
     );
   }
 
-  if (error) { return <h4>{error.message}</h4>;}
+  if (error) { return <h4>Oops Something Went Wrong{error.message}</h4>; }
 
   const handleCategoryChange = (category) => {
     setSelectedCategory((previousCategory) =>
@@ -32,138 +31,102 @@ const Products = () => {
     );
   };
 
-  const filteredProducts = selectedCategory?products.filter((item) => item.category === selectedCategory):products;
+  const filteredProducts = selectedCategory ? products.filter((item) => item.category === selectedCategory) : products;
 
   return (
     <Layout>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          minHeight: '100vh', // Ensures content takes up the full height
-          padding: '20px',
-        }}
-      >
+      <div style={styles.productContainer}>
+
         {/* Sidebar with filters */}
-        <Paper
-          sx={{
-            width: '20%',
-            margin: '10px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            padding: '15px',
-            boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
-            backgroundColor: '#f9f9f9',
-            minHeight: '100vh',
-          }}
-        >
-           
-          <h6 style={{color:"#c30c2c"}}>Filter By Category</h6>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={selectedCategory === 'Men'}
-                onChange={() => handleCategoryChange('Men')}
-              />
-            }
-            label="Men"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={selectedCategory === 'Women'}
-                onChange={() => handleCategoryChange('Women')}
-              />
-            }
-            label="Women"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={selectedCategory === 'Kids'}
-                onChange={() => handleCategoryChange('Kids')}
-              />
-            }
-            label="Kids"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={selectedCategory === 'Sports'}
-                onChange={() => handleCategoryChange('Sports')}
-              />
-            }
-            label="Sports"
-          />
-          
+        <Paper sx={styles.productSidebar}>
+            <h6 style={{ color: "#c30c2c" }}>Filter By Category</h6>
+            <FormControlLabel control={<Checkbox checked={selectedCategory === 'Men'} onChange={() => handleCategoryChange('Men')} />} label="Men" />
+            <FormControlLabel control={<Checkbox checked={selectedCategory === 'Women'} onChange={() => handleCategoryChange('Women')} />} label="Women" />
+            <FormControlLabel control={<Checkbox checked={selectedCategory === 'Kids'} onChange={() => handleCategoryChange('Kids')} />} label="Kids" />
+            <FormControlLabel control={<Checkbox checked={selectedCategory === 'Sports'} onChange={() => handleCategoryChange('Sports')} />} label="Sports" />
         </Paper>
 
         {/* Products List */}
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            gap: '20px',
-            margin: '30px',
-            padding: '10px',
-            flexGrow: 3,
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            minHeight: '100vh',
-            overflowY: 'auto', // Allows content to scroll if it overflows
-            paddingBottom: '50px', // Adds extra space to prevent footer overlap
-          }}
-        >
+        <div style={styles.productList}>
           {filteredProducts.map((item) => (
-            
-            <div
-              key={item._id}
-              style={{
-                border: '1px solid #ddd',
-                borderRadius: '8px',
-                padding: '10px',
-                width: '230px',
-                maxHeight: '350px',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                margin:"5px"
-              }}
-            >
-              <img
-                src={`${process.env.REACT_APP_API}/uploads/${item.image}`}
-                alt={item.title}
-                width={200}
-                height={200}
-                style={{ objectFit: 'cover', marginBottom: '10px' }}
-              />
+            <div key={item._id} style={styles.productItem} >
+              <img src={`${process.env.REACT_APP_API}/uploads/${item.image}`} alt={item.title} style={styles.productImage} />
               <h6>{item.title}</h6>
-              <p style={{ fontSize: '15px', marginBottom: '0'}}>
-                Price: ${item.price}
-              </p>
-              <p style={{ fontSize: '15px' }}>Category: {item.category}</p>
-              <button onClick={()=>dispatch(addToCart(item))}
-                style={{
-                  background: '#353935',
-                  color: 'white',
-                  border: 'none',
-                  outline: 'none',
-                  width: '200px',
-                  padding: '5px',
-                  marginTop: 'auto',
-                  borderRadius:"5px"
-                }} 
-               >
-                Add To Cart
-              </button>
+              <p style={styles.productText}>Price: ${item.price}</p>
+              <p style={styles.productText}>Category: {item.category}</p>
+              <button style={styles.productButton} onClick={() => dispatch(addToCart(item))}>Add To Cart</button>
             </div>
-            
           ))}
         </div>
+
       </div>
     </Layout>
   );
 };
 
 export default Products;
+
+//Styles
+const styles = {
+  productContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    minHeight: '100vh',
+    padding: '20px',
+  },
+  productSidebar: {
+    width: '20%',
+    margin: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    padding: '15px',
+    boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+    backgroundColor: '#f9f9f9',
+    minHeight: '100vh',
+  },
+  productList: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '20px',
+    margin: '30px',
+    padding: '10px',
+    flexGrow: 3,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    minHeight: '100vh',
+    overflowY: 'auto',
+    paddingBottom: '50px',
+  },
+  productItem: {
+    border: '1px solid #ddd',
+    borderRadius: '8px',
+    padding: '10px',
+    width: '230px',
+    maxHeight: '350px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: "5px"
+  },
+  productButton: {
+    background: '#353935',
+    color: 'white',
+    border: 'none',
+    outline: 'none',
+    width: '200px',
+    padding: '5px',
+    marginTop: 'auto',
+    borderRadius: "5px"
+  },
+  productImage: {
+    objectFit: 'cover',
+    marginBottom: '10px',
+    width: "200px",
+    height: "200px"
+  },
+  productText: {
+    fontSize: '15px',
+    marginBottom: '5px'
+  }
+}
