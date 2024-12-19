53acf73546e526a993fa9f026a029c80019d2410
Rachit Dhyani <rachitdhyani2002@gmail.com>
2024-12-19 11:21:29 +0530

    Updated Success.js

--- client/src/pages/screens/Success.js
+++ client/src/pages/screens/Success.js
@@ -2,24 +2,62 @@ import React from 'react'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { Box, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+
+
 const Success = () => {
-    const navigate = useNavigate();
-    const handleSubmit=()=>{
-        navigate("/home")
-    }
+
+  //UseNavigate Hook
+  const navigate = useNavigate();
+
+  //Navigate Home On Success
+  const handleSubmit = () => {navigate("/home")}
+
   return (
     <div>
-       <Box sx={{display:"flex",justifyContent:"center",marginTop:"80px"}}>
-        <Paper sx={{width:"500px",margin:"20px"}}>
-            <CheckCircleIcon sx={{color:"#2cff05",fontSize:"100px",margin:'30px'}} />
-            <h2>Payment Successful</h2>
-            <p style={{marginTop:"20px",margin:"30px"}}>Thank you for your payment.Your transaction has been completed successfully</p>
-            <button style={{padding:"10px",border:"none",outline:"none",background:'#2cff05',color:"white",width:"180px",borderRadius:"8px",marginBottom:"40px"}} onClick={handleSubmit}>Go to homepage</button>
+       
+      <Box sx={styles.successContainer}>
+        <Paper sx={styles.successBox}>
+          <CheckCircleIcon sx={styles.successIcon} />
+          <h2>Payment Successful</h2>
+          <p style={styles.successMessage}>Thank you for your payment.Your transaction has been completed successfully</p>
+          <button style={styles.successButton} onClick={handleSubmit}>Go to homepage</button>
         </Paper>
-        </Box>      
+      </Box>
+
     </div>
-    
+
   )
 }
-
 export default Success
+
+//Styles
+const styles = {
+  successContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "80px"
+  },
+  successBox: {
+    width: "500px",
+    margin: "20px"
+  },
+  successIcon: {
+    color: "#2cff05",
+    fontSize: "100px",
+    margin: '30px'
+  },
+  successMessage: {
+    marginTop: "20px",
+    margin: "30px"
+  },
+  successButton: {
+    padding: "10px",
+    border: "none",
+    outline: "none",
+    background: '#2cff05',
+    color: "white",
+    width: "180px",
+    borderRadius: "8px",
+    marginBottom: "40px"
+  }
+}
