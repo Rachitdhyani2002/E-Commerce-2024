6002c2376150c5088771d358452f8f659feedb49
Rachit Dhyani <rachitdhyani2002@gmail.com>
2024-12-20 11:16:22 +0530

    Update Cancel.js

--- client/src/pages/screens/Cancel.js
+++ client/src/pages/screens/Cancel.js
@@ -3,23 +3,61 @@ import CancelIcon from '@mui/icons-material/Cancel';
 import { Box, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 const Cancel = () => {
-    const navigate = useNavigate();
-    const handleSubmit=()=>{
-        navigate("/home")
-    }
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    navigate("/home")
+  }
   return (
     <div>
-       <Box sx={{display:"flex",justifyContent:"center",marginTop:"80px"}}>
-        <Paper sx={{width:"500px",margin:"20px"}}>
-            <CancelIcon sx={{color:"red",fontSize:"100px",margin:'30px'}} />
-            <h2>Payment UnSuccessful!</h2>
-            <p style={{marginTop:"20px",margin:"30px"}}>Sorry! your payment failed.Please try again later</p>
-            <button style={{padding:"10px",border:"none",outline:"none",background:'red',color:"white",width:"180px",borderRadius:"8px",marginBottom:"40px"}} onClick={handleSubmit}>Go to Homepage</button>
+      <Box sx={styles.cancelContainer}>
+        <Paper sx={styles.cancelBox}>
+          <CancelIcon sx={styles.cancelIcon} />
+          <h2>Payment UnSuccessful!</h2>
+          <p style={styles.failMessage}>Sorry! your payment failed.Please try again later</p>
+          <button style={styles.button} onClick={handleSubmit}>Go to Homepage</button>
         </Paper>
-        </Box>      
+      </Box>
     </div>
-    
+
   )
 }
 
 export default Cancel
+
+//Styles
+const styles = {
+  cancelContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "80px",
+    
+  },
+  cancelBox: {
+    width: "500px",
+    margin: "20px",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection:"column",
+    alignItems:"center"
+  },
+  cancelIcon: {
+    color: "red",
+    fontSize: "100px",
+    margin: '30px'
+  },
+  failMessage: {
+    marginTop: "20px",
+    margin: "30px"
+  },
+  button: {
+    padding: "10px",
+    border: "none",
+    outline: "none",
+    background: 'red',
+    color: "white",
+    width: "180px",
+    borderRadius: "8px",
+    marginBottom: "40px"
+  }
+
+}
