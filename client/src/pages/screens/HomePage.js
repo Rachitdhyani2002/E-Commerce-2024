7540f1c5c58c577d3d5c2532d5e6885b4f139326
Rachit Dhyani <rachitdhyani2002@gmail.com>
2024-12-21 18:43:04 +0530

    Added resposiveness

--- client/src/pages/screens/HomePage.js
+++ client/src/pages/screens/HomePage.js
@@ -12,8 +12,6 @@ import image5 from '../../utils/images/Neeraj-Chopra-Diamnd-League-Trophy.jpg'
 import Cookies from 'js-cookie'
 
 
-
-
 const Homepage = () => {
   const user = localStorage.getItem('user');
   const token = Cookies.get('refreshToken')
@@ -26,7 +24,6 @@ const Homepage = () => {
     }
   }, [user, token, navigate]);
 
-  //Handle Navigation Function
   const handleNavigation = () => { navigate('/products') }
 
 
@@ -78,45 +75,45 @@ const Homepage = () => {
         </Box>
       </Box>
       {/* Container3 */}
-      
 
-       {/* Container4 */}
+
+      {/* Container4 */}
       <Box sx={styles.newContainer}>
-        <Box sx={{ display: "flex", justifyContent: 'center', flexDirection: "column", width: "35%" }}>
+        <Box sx={styles.newContainerText}>
           <h1 style={styles.newContainerHeadingFirst}>NEW LOOK FOR YOUR WATCH</h1>
           <h4 style={styles.newContainerHeadingSecond}>Your watch and strap should go together perfectly, but that doesn't mean you can't experiment with different combinations. Why not mix things up and see what happens? You may discover a look that's as individual as you are.</h4>
           <Button onClick={handleNavigation} sx={styles.newContainerButton}>SHOP THE COLLECTION</Button>
         </Box>
         <img src={gif1} width={400} height={400} />
       </Box>
-       {/* Container4 */}
+      {/* Container4 */}
 
 
-          {/* Container5 */}
+      {/* Container5 */}
       <Box sx={styles.newsContainer}>
         <h1 style={styles.newsContainerHeadingFirst}>NEWS & STORIES</h1>
         <Box sx={styles.newsContainerBox}>
           <Box sx={styles.newsContainerBoxFirst}>
-              <img src={image3} width={400} height={300} style={{ borderRadius: '10px' }} />
+              <img src={image3} width={400} height={300} style={{ width: "100%", height: "auto" }} />
               <p style={styles.newsContainerBoxFirstHeadingOne}>SPORTS</p>
               <h4 style={styles.newsContainerBoxFirstHeadingSecond}>WILL CHARLES WINS IN LONDON</h4>
               <p style={styles.newsContainerBoxFirstHeadingThird}>Odysseus CEO WILL CHARLES has finished the golf season as the race to London Champion</p>
           </Box>
           <Box sx={styles.newsContainerBoxSecond}>
-              <img src={image4} width={400} height={300} style={{ borderRadius: '10px' }} />
+              <img src={image4} width={400} height={300} style={{ width: "100%", height: "auto" }} />
               <p style={styles.newsContainerBoxSecondHeadingFirst}>SPORTS</p>
               <h4 style={styles.newsContainerBoxFirstHeadingSecond}>ODYSSEUS IS HOSTING SPORT CUP </h4>
               <p style={styles.newsContainerBoxFirstHeadingThird}>After the successful host of golfing events now we are hosting charitable sports events</p>
           </Box>
           <Box sx={styles.newsContainerBoxThird}>
-              <img src={image5} width={400} height={300} style={{ borderRadius: '10px' }} />
+              <img src={image5} width={400} height={300} style={{ width: "100%", height: "auto" }} />
               <p style={styles.newsContainerBoxThirdHeadingFirst}>SPORTS</p>
               <h4 style={styles.newsContainerBoxThirdHeadingSecond}>NeeRAJ JOINS ODYSSEUS WATCHES</h4>
               <p style={styles.newsContainerBoxThirdHeadingThird}>NeeRaj Chopra is going to be the new face of our ODYSSEUS watches</p>
           </Box>
         </Box>
       </Box>
-       {/* Container5*/}
+      {/* Container5 */}
 
     </Layout>
   );
@@ -129,36 +126,63 @@ const styles = {
   container: {
     display: "flex",
     flexDirection: "column",
+    justifyContent: "center",
     padding: "40px",
-    margin: "40px"
+    margin: "40px",
+    '@media (max-width: 600px)': {
+      padding: "20px",
+      margin: "20px",
+      alignItems: "center",
+    },
   },
   collection: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     padding: "40px",
     margin: "20px",
-    textAlign:"center"
+    textAlign: "center",
+    '@media (max-width: 600px)': {
+      padding: "20px",
+      margin: "10px",
+    },
   },
   collectionHeadingFirst: {
     color: "#2E2E2E",
     fontWeight: "100",
     marginBottom: "30px",
     fontSize: "20px",
-    fontFamily: "font-family: 'Futura', 'Avenir', sans-serif",
-    letterSpacing: "5px"
+    fontFamily: "sans-serif",
+    letterSpacing: "5px",
+    '@media (max-width: 600px)': {
+      padding: "20px",
+      margin: "10px",
+    },
+
   },
   collectionHeadingSecond: {
     color: '#c30c2c',
     fontSize: "40px",
     fontWeight: "400",
-    fontFamily: "font-family: 'Futura', 'Avenir', sans-serif",
-    letterSpacing: '8px'
+    fontFamily: "sans-serif",
+    letterSpacing: '8px',
+    '@media (max-width: 600px)': {
+      fontSize: "10px",
+      letterSpacing: '1px',
+    },
   },
   collectionHeadingThird: {
     fontWeight: "300",
     fontSize: "20px",
     marginTop: "40px",
-    fontFamily: "font-family: 'Futura', 'Avenir', sans-serif",
+    fontFamily: "sans-serif",
     color: "#2E2E2E",
-    letterSpacing: '1px'
+    letterSpacing: '1px',
+    '@media (max-width: 600px)': {
+      fontSize: "13px",
+      marginTop: "20px",
+    }
   },
   collectionButton: {
     border: "1px solid grey",
@@ -175,21 +199,43 @@ const styles = {
       boxShadow: 'none',
       border: "none",
       transition: "background-color 0.8s ease-in"
-    }
+    },
+    '@media (max-width: 600px)': {
+      width: "250px",
+      height: "50px",
+    },
   },
   watchImageBox: {
     display: "flex",
     flexDirection: "row",
     gap: "50px",
     marginTop: "50px",
-    justifyContent: "center"
+    justifyContent: "center",
+    '@media (max-width: 600px)': {
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
   },
   watchImageBoxImageFirst: {
-    marginTop: '200px',
-    borderRadius: "10px"
+    margin: "10px",
+    maxWidth: "100%",
+    borderRadius: "10px",
+    height: "auto",
+    '@media (max-width: 600px)': {
+      maxWidth: "90%",
+      height: "auto",
+    },
   },
   watchImageBoxImageSecond: {
-    borderRadius: "10px"
+    margin: "10px",
+    maxWidth: "100%",
+    height: "auto",
+    borderRadius: "10px",
+    '@media (max-width: 600px)': {
+      maxWidth: "90%",
+      height: "auto",
+    },
   },
   giftCollectionBox: {
     display: "flex",
@@ -197,13 +243,24 @@ const styles = {
     padding: "30px",
     margin: "40px",
     justifyContent: "center",
-    gap: "40px"
+    gap: "40px",
+    '@media (max-width: 600px)': {
+      flexDirection: "column",
+      padding: "15px",
+      margin: "20px",
+      gap: "20px",
+      alignItems: "center",
+    },
   },
   giftCollectionBoxText: {
     display: "flex",
     justifyContent: 'center',
     flexDirection: "column",
-    width: "35%"
+    width: "35%",
+    '@media (max-width: 600px)': {
+      width: "100%",
+      textAlign: "center"
+    },
   },
   giftCollectionBoxTextHeadingFirst: {
     color: '#c30c2c',
@@ -211,7 +268,8 @@ const styles = {
     fontWeight: "400",
     fontFamily: "font-family: 'Futura', 'Avenir', sans-serif",
     letterSpacing: '8px',
-    textAlign: "left"
+    textAlign: "left",
+
   },
   giftCollectionBoxTextHeadingSecond: {
     fontWeight: "300",
@@ -231,12 +289,11 @@ const styles = {
     marginTop: "20px",
     width: "320px",
     height: "60px",
-    '&:hover': { backgroundColor: '#c30c2c',
-                color: "white",
-                boxShadow: 'none',
-                border: "none",
-                transition: "background-color 0.8s ease-in"
-               }
+    '&:hover': { backgroundColor: '#c30c2c', color: "white", boxShadow: 'none', border: "none", transition: "background-color 0.8s ease-in" },
+    '@media (max-width: 600px)': {
+      width: "100%",
+      height: "50px"
+    },
   },
   aboutUsContainer: {
     display: 'flex',
@@ -245,7 +302,15 @@ const styles = {
     margin: "60px",
     height: '600px',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    '@media (max-width: 600px)': {
+      flexDirection: 'column',
+      padding: "20px",
+      margin: "20px",
+      height: 'auto',
+      alignItems: "center",
+      textAlign: "center"
+    },
   },
   aboutUsContainerHeading: {
     p: 10,
@@ -254,12 +319,18 @@ const styles = {
     alignItems: "center",
     flexDirection: 'column',
     textAlign: "left",
-    background: 'white'
+    background: 'white',
+    '@media (max-width: 600px)': {
+      textAlign: "center",
+      width: "100%",
+      height: "200px",
+    },
   },
   aboutUsContainerHeadingFirst: {
     color: "#c30c2c",
     fontSize: "80px",
     fontWeight: "100",
+
   },
   aboutUsContainerHeadingSecond: {
     marginTop: "1px",
@@ -270,7 +341,12 @@ const styles = {
     background: 'white',
     width: '200',
     margin: "20px",
-    padding: "50px"
+    padding: "50px",
+    '@media (max-width: 600px)': {
+      width: '100%',
+      margin: "10px",
+      padding: "20px",
+    },
   },
   newContainer: {
     display: "flex",
@@ -278,7 +354,30 @@ const styles = {
     padding: "30px",
     marginBottom: "80px",
     justifyContent: "center",
-    gap: "40px"
+    gap: "40px",
+    '@media (max-width: 600px)': {
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+      padding: "20px",
+    },
+  },
+  newContainerText: {
+    display: "flex",
+    justifyContent: 'center',
+    flexDirection: "column",
+    width: "35%",
+    '@media (max-width: 600px)': {
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+      height: "auto",
+      padding: "20px",
+      margin: "20px",
+      gap: "20px",
+    },
   },
   newContainerHeadingFirst: {
     color: '#c30c2c',
@@ -312,7 +411,12 @@ const styles = {
       boxShadow: 'none',
       border: "none",
       transition: "background-color 0.8s ease-in"
-    }
+    },
+    '@media (max-width: 600px)': {
+      width: "100%", // Full width button on smaller screens
+      height: "50px", // Reduce height
+      marginTop: "10px", // Smaller margin
+    },
   },
   newsContainer: {
     display: 'flex',
@@ -320,7 +424,14 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: "150px",
-    marginBottom: "100px"
+    marginBottom: "100px",
+    '@media (max-width: 600px)': {
+      marginTop: "100px",
+      marginBottom: "50px",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center"
+    },
   },
   newsContainerHeadingFirst: {
     color: '#c30c2c',
@@ -335,17 +446,31 @@ const styles = {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    gap: "20px"
+    gap: "20px",
+    '@media (max-width: 600px)': {
+      flexDirection: 'column',
+      gap: "15px",
+      width: "80%",
+      padding: "10px",
+      margin: "10px",
+    },
   },
   newsContainerBoxFirst: {
     padding: "10px",
-    width: '30%'
+    width: '30%',
+    '@media (max-width: 600px)': {
+      width: '100%',
+    },
   },
   newsContainerBoxFirstHeadingOne: {
     color: "#c30c2c",
     textAlign: "left",
     fontSize: '12px',
-    marginTop: "15px"
+    marginTop: "15px",
+    '@media (max-width: 600px)': {
+      fontSize: "10px",
+      textAlign: "center",
+    },
   },
   newsContainerBoxFirstHeadingSecond: {
     color: "#c30c2c",
@@ -360,7 +485,10 @@ const styles = {
   },
   newsContainerBoxSecond: {
     padding: "10px",
-    width: '30%'
+    width: '30%',
+    '@media (max-width: 600px)': {
+      width: '100%',
+    },
   },
   newsContainerBoxSecondHeadingFirst: {
     color: "#c30c2c",
@@ -381,7 +509,10 @@ const styles = {
   },
   newsContainerBoxThird: {
     padding: "10px",
-    width: '30%'
+    width: '30%',
+    '@media (max-width: 600px)': {
+      width: '100%'
+    },
   },
   newsContainerBoxThirdHeadingFirst: {
     color: "#c30c2c",
@@ -399,5 +530,6 @@ const styles = {
     textAlign: "left",
     color: "#2E2E2E",
     marginTop: "20px"
-  }
+  },
+
 } 
